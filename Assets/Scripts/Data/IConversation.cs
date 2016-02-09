bb4779a172b1314b765ac91a2d8a4c1af9478bd9
imann24 <imann24@gmail.com>
2016-02-09 17:57:32 -0500

    Adding interfacs for Conversation class

---
+++ Assets/Scripts/Data/IConversation.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Author: Isaiah Mann
+ * Description: Abstract representation of the conversation class
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public interface IConversation {
+
+	// This first line of dialogue in this conversation
+	IConversationNode GetFirstDialogue ();
+
+	// The current line of dialogue, this is changed when you call AdvanceDialogue
+	IConversationNode GetCurrentDialogue ();
+
+	// Can call the corresponding method inside IConversationNode
+	string GetCurrentDialogueText ();
+
+	// Sets the Dialogue to the new IConversationNode, if it is a valid response
+	void AdvanceDialogue (IConversationNode Response);
+}
---
+++ Assets/Scripts/Data/IConversationNode.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Author: Isaiah Mann
+ * Decription: Abstract representation of a single line of dialogue
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public interface IConversationNode {
+
+	// Responses to this line of dialogue (there could only be one response in some cases)
+	IList GetResponses ();
+
+	// The actual text of this dialogue
+	string GetText ();
+
+	// Whether or not there are any responses this node (false if it is the last line in a conversatino
+	bool HasResponses ();
+}
