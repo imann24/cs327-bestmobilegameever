4b81e5ecd038e1c5f33b45e5018623d272e4c2f6
pan23h <donna_world@n18-133.mtholyoke.edu>
2016-02-09 18:46:55 -0500

    made basic inventory
    made basic item database, including Item class and ItemDatabase
    (inventory). reading from xml must be discussed

---
+++ Assets/Scripts/Data/ItemDatabase/ItemDatabase.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Author: Donna Pan
+ * Description: A class that holds all the items
+ * Dependencies: Item.cs
+ */
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class ItemDatabase : MonoBehaviour {
+
+	public List<Item> items = new List<Item>();
+
+	// stub for now; should read item information from the controller(?) in a certain format
+	// and addItem to the list of items
+	// But we should just do it via the Unity interface...
+	public void initItemDatabase() {
+		throw new System.NotImplementedException();
+	}
+
+	public Item getItem(string itemName) {
+		return items.Find(x => x.itemName == itemName);
+	}
+		
+	private void addItem(Item item) {
+		items.Add (item);
+	}
+	
+}
+
