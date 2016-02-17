2df74abd1d953e092aa0dcdb17baa5c2dd6b3939
Michael Forrest <mikemforrest@gmail.com>
2016-02-17 11:40:13 -0500

    Added Dialog Placeholders, VisualConversationDisplayer Classs
    VCD is in progress

---
+++ Assets/Scripts/Data/VisualConversationDisplayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI
+
+public class VisualConversationDisplayer : MonoBehaviour {
+
+	public Conversation CurrentConvo;
+
+	public VisualConversationDisplayer (Conversation c){
+		CurrentConvo = c;
+	}
+
+	public void BeginConvo(){
+
+	}
+
+	public void AdvanceConvo(){
+
+	}
+
+	public void EndConvo(){
+
+	}
+
+
+}
