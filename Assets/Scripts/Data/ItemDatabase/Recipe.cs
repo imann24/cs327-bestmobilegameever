37e56d7fa8e01498d27d651248a7af620d7a18f8
Donna <pan23h@mtholyoke.edu>
2016-02-17 13:03:47 -0500

    Items related updates with same XML
    Items & XML

---
+++ Assets/Scripts/Data/ItemDatabase/Recipe.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Author: Donna Pan
+ * Description: A class that holds recipes.
+ * Dependencies: --
+ */
+
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Recipe : MonoBehaviour {
+	
+	public string Objective;
+	public List<Dictionary<string,int>> Ingredients = new List<Dictionary<string,int>>();
+
+	public Recipe(DataNode recipe) {
+		Objective = recipe [0].Value;
+		int i = 1;
+		while (recipe [i] != null) {
+			Dictionary<string,int> ingredient = new Dictionary<string,int> (recipe [i] [0], recipe [i] [1]);
+			Ingredients.Add (ingredient);
+		}
+	}
+}
