da64c338fe0851df8b4ab1bb96515c290b9bb614
Jowell-E <jjoelesquilin@gmail.com>
2016-02-17 17:47:24 -0500

    Initial World Scene
    -Player character can move to location clicked or object clicked.
    -Clickable Tag on object calls blank OnTouch functions which can be
    filled with interaction features
    -Work in Progress will need to coordinate with other peeps

---
+++ Assets/Scripts/Controller/InputController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : MonoBehaviour {
+	public PlayerMovement player;
+
+	private Camera mainCamera;
+
+	void Start(){
+		mainCamera = this.gameObject.GetComponent<Camera> (); 
+	}
+
+	// Update is called once per frame
+	void Update () {
+		#if UNITY_EDITOR
+		if (Input.GetMouseButton(0) || Input.GetMouseButtonDown(0) || Input.GetMouseButtonUp(0)) {
+
+			Ray ray = mainCamera.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast (ray, out hit)) {
+				GameObject recipient = hit.transform.gameObject;
+
+				if (recipient.tag == "Clickable") {
+					if (Input.GetMouseButtonDown(0)) {
+						recipient.SendMessage ("OnTouchDown", SendMessageOptions.DontRequireReceiver);
+					}
+					if (Input.GetMouseButtonUp(0)) {
+						recipient.SendMessage ("OnTouchUp", SendMessageOptions.DontRequireReceiver);
+					}
+					if (Input.GetMouseButton(0)) {
+						recipient.SendMessage ("OnTouchStay", SendMessageOptions.DontRequireReceiver);
+					}
+				}
+				else if (recipient.tag == "Ground"){
+					Debug.Log (mainCamera.ScreenToWorldPoint(Input.mousePosition).x);
+					player.MoveTowards(new Vector2 (mainCamera.ScreenToWorldPoint(Input.mousePosition).x, mainCamera.ScreenToWorldPoint(Input.mousePosition).y) , false);
+				}
+			}
+		}
+		#endif
+		if (Input.touchCount > 0) {
+
+			foreach (Touch touch in Input.touches) {
+				Ray ray = mainCamera.ScreenPointToRay (touch.position);
+				RaycastHit hit;
+
+				if (Physics.Raycast (ray, out hit)) {
+					GameObject recipient = hit.transform.gameObject;
+
+					if (recipient.tag == "Clickable") {
+						if (touch.phase == TouchPhase.Began) {
+							recipient.SendMessage ("OnTouchDown", SendMessageOptions.DontRequireReceiver);
+						}
+						if (touch.phase == TouchPhase.Ended) {
+							recipient.SendMessage ("OnTouchUp", SendMessageOptions.DontRequireReceiver);
+						}
+						if (touch.phase == TouchPhase.Stationary) {
+							recipient.SendMessage ("OnTouchStay", SendMessageOptions.DontRequireReceiver);
+						}
+						if (touch.phase == TouchPhase.Canceled) {
+							recipient.SendMessage ("OnTouchExit", SendMessageOptions.DontRequireReceiver);
+						}
+					} 
+				} 
+			}
+		}
+	}
+}
--- Assets/Scripts/Data/CharacterClass.cs
+++ Assets/Scripts/Data/CharacterClass.cs
@@ -1,24 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[System.Serializable]
 public class CharacterClass : MonoBehaviour {
 
 	private static DataTree characterList = DataUtil.ParseXML("Text/CharacterList");
 
-	string[,] peopleList = new string[2, 5]; // Amount of characters and max number of spites + 1  for each character
+	public string characterName;
+	private static string[,] peopleList = new string[2, 5]; // Amount of characters and max number of spites + 1  for each character
 	// [0, 0] serves as the name of each other where [0, 1] is the first sprite
+
+	string[] sprites = new string[peopleList.GetLength(1) - 1];
+	private SpriteRenderer spriteRenderer;
 
 	void Start(){
 		GetPeople ();
 		 
 		//indivdual sprite list for character via name
-		string[] person = new string[peopleList.GetLength(1) - 1];
-		person = GetSprites ("Bob"); 
-
-
-		foreach (string sprite in person) {
-			Debug.Log (sprite);
-		}
+		sprites = GetSprites (name); 
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		Sprite currentSprite = (Sprite) Resources.Load (sprites [0], typeof(Sprite)); 
+		spriteRenderer.sprite = currentSprite;
 	}
 
 	public void GetPeople(){  //Initializes list of people and their sprites
