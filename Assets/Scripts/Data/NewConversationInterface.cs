ebfd029087ff99a6546149776f497dbd6c1a5f86
heyitskaya <ni23x@mtholyoke.edu>
2016-02-17 14:18:19 -0500

    New and improved non-generic Conversation Interface

---
+++ Assets/Scripts/Data/NewConversationInterface.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public interface ConversationInterface
+{
+
+	/** get the first line of dialogue in the conversation**/
+	DirectedGraphNode<string> GetFirstDialogue();
+
+	/** get the current node we are at**/
+	DirectedGraphNode<string> GetCurrentDialogue();
+
+	/** get the current dialogue text**/
+	string GetCurrentDialogueText();
+
+	/** Sets the Dialogue to the new IConversationNode, if it is a valid response **/
+	void AdvanceDialogue(DirectedGraphNode<string> Response);
+
+
+
+
+}
+
+
