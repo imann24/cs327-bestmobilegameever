b78fcd5673e208133015e6b9d1307af5d92fabc0
NC8815 <nopc15@hampshire.edu>
2016-02-17 03:33:36 -0500

    Adding UI skeleton

---
+++ Assets/InvItem.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InvItem : MonoBehaviour {
+
+	public string itemName;
+	public Texture2D itemTexture;
+	public Sprite itemSprite { get { return Sprite.Create (itemTexture, new Rect (0, 0, itemTexture.width, itemTexture.height), Vector2.zero); } }
+	public InvSlot itemSlot { get; private set; }
+	// Use this for initialization
+
+	void OnMouseUpAsButton(){
+		if (Inventory.Instance.selectedItem == null) { //If you click on an item in the screen and don't currently have one selected...
+			Inventory.Instance.AddItem (this);     //add it to inventory
+		}
+	}
+
+	public void PickUp(InvSlot slot){
+		gameObject.GetComponent<SpriteRenderer> ().enabled = false;//stop showing it in the screen
+		gameObject.GetComponent<Collider> ().enabled = false;//stop being able to click on it
+		itemSlot = slot;//record where we put it.
+	}
+
+	public void Drop(){
+		this.transform.position = Camera.main.ScreenToWorldPoint (Input.mousePosition) + Vector3.forward;
+		gameObject.GetComponent<SpriteRenderer> ().enabled = true;
+		gameObject.GetComponent<Collider> ().enabled = true;
+		itemSlot = null;
+	}
+}
--- Assets/Scripts/Data/ConversationInterface.cs
+++ Assets/Scripts/Data/ConversationInterface.cs
@@ -2,25 +2,25 @@
 using UnityEngine;
 using System.Collections;
 
-
+/**
 public interface ConversationInterface
 {
 
-	/** get the first line of dialogue in the conversation**/
+	/** get the first line of dialogue in the conversation
 	DirectedGraphNode<T> GetFirstDialogue();
 
-	/** get the current node we are at**/
+	/** get the current node we are at
 	DirectedGraphNode<T> GetCurrentDialogue();
 
-	/** get the current dialogue text**/
+	/** get the current dialogue text
 	string GetCurrentDialogueText();
 
-	/** Sets the Dialogue to the new IConversationNode, if it is a valid response **/
+	/** Sets the Dialogue to the new IConversationNode, if it is a valid response
 	void AdvanceDialogue(DirectedGraphNode<T> Response);
 
 
 
 
-}
+}**/
 
 
