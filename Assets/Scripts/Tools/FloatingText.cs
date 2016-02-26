f922c836055575cf97a72f12d4889bbc9f5ee0fa
NC8815 <nopc15@hampshire.edu>
2016-02-26 02:52:23 -0500

    Increased Interaction Functionality/Documentation, Culled obsolete interaction scripts
    There were some scripts from a previous version of the interactable
    system I was working on. They were no longer necessary, and didn't
    contain any useful code not already somewhere else. I updated the
    functionality of the interactions available to reflect a discussion with
    design.

---
+++ Assets/Scripts/Tools/FloatingText.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FloatingText : MonoBehaviour {
+
+	public float BaseLifeSpan = 1;
+	public float ScrollSpeed = 5;
+	public float startToFade = 3;
+	float remainingLifeSpan;
+	Color baseColor;
+	Color finalColor { get { return new Color (baseColor.r, baseColor.g, baseColor.g, 0); } }
+
+	// Use this for initialization
+	void Start () {
+		baseColor = GetComponent<Text> ().color;
+		remainingLifeSpan = BaseLifeSpan;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		remainingLifeSpan -= Time.deltaTime;
+		GetComponent<RectTransform> ().localPosition += Vector3.up * ScrollSpeed * Time.deltaTime;
+		if (remainingLifeSpan < 0) {
+			Destroy (gameObject);
+		}
+		if (remainingLifeSpan < startToFade) {
+			GetComponent<Text> ().color = Color.Lerp (finalColor, baseColor, remainingLifeSpan / startToFade);
+		}
+	}
+}
---
+++ Assets/Scripts/Tools/SpeechBubble.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SpeechBubble : MonoBehaviour {
+	void Update () {
+		transform.LookAt (transform.position + Camera.main.transform.rotation * Vector3.forward, Camera.main.transform.rotation * Vector3.up);
+	}
+
+	public void Say(string textToSay){
+		GameObject go = Instantiate (Resources.Load<GameObject> ("Prefabs/FloatingText"));
+		go.GetComponent<Text> ().text = textToSay;
+		go.transform.SetParent (transform);
+		go.transform.localScale = Vector3.one;
+		go.GetComponent<RectTransform> ().localPosition = Vector3.zero;
+	}
+}
