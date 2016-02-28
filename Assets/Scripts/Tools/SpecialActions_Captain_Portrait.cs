ec7afb98636d1c56ca7d3d040d9a30578f3a2019
Sienna Cornish <sc16@hampshire.edu>
2016-02-28 18:15:33 -0500

    Added Captain Portrait prefab & placeholder xml. Added Rag prefab & placeholder xml.

---
+++ Assets/Scripts/Tools/SpecialActions_Captain_Portrait.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpecialActions_Captain_Portrait : SpecialActions {
+    public Sprite CaptainPortraitPainted;
+    public Sprite CaptainPortraitDressed;
+
+    public override void DoSpecialAction(string actionTag) {
+        switch (actionTag) {
+            case "ChangeToPainted":
+                GetComponent<SpriteRenderer>().sprite = CaptainPortraitPainted;
+                if (gameObject.GetComponent<Interactable>().Debugging) { Debug.Log("Attempting to paint."); }
+                break;
+            case "ChangeToDressed":
+                GetComponent<SpriteRenderer>().sprite = CaptainPortraitDressed;
+                if (gameObject.GetComponent<Interactable>().Debugging) { Debug.Log("Attempting to dress."); }
+                break;
+            default:
+                if (gameObject.GetComponent<Interactable>().Debugging) {
+                    Debug.Log(actionTag + " isn't defined in SpecialActions_Captain_Portrait.");
+                }
+                break;
+        }
+    }
+}
