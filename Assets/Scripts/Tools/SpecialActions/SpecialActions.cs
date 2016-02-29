3a8dd47f031316f920ce4b22f8d91bcc786d109e
Sienna Cornish <sc16@hampshire.edu>
2016-02-29 13:34:54 -0500

    Moved special actions into their own folder.

---
+++ Assets/Scripts/Tools/SpecialActions/SpecialActions.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpecialActions : MonoBehaviour {
+
+	public void DoSpecialActions(List<string> actionList){
+		bool destroy = false;
+		foreach (string action in actionList) {
+			switch (action) {
+			case "ReturnSelected":
+				InventoryManager.Instance.ReturnSelected ();
+				break;
+			case "Destroy":
+				destroy = true;
+				break;
+			case "ComeHere":
+				GameManager.Instance.playerCharacter.GetComponent<Movement> ().MoveTo (transform.position);
+				break;
+			default:
+				DoSpecialAction (action);
+				break;
+			}
+		}
+		if (destroy) {
+			Destroy (gameObject);
+		}
+	}
+
+	//override this function in subclasses for specific actions.
+	public virtual void DoSpecialAction(string actionTag){
+		if (gameObject.GetComponent<Interactable> ().Debugging) {
+			Debug.Log (actionTag + " isn't defined here. This object has the base SpecialActions behavior.");
+		}
+	}
+
+    public void ChangeSprite(Sprite newSprite) {
+        GetComponent<SpriteRenderer>().sprite = newSprite;
+    }
+}
---
+++ Assets/Scripts/Tools/SpecialActions/SpecialActions_Cutscene_Tutorial.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpecialActions_Cutscene_Tutorial : SpecialActions {
+    public GameObject Quartermaster;
+    public GameObject Shipmaster;
+    public GameObject Firstmate;
+    public Vector2 QuartermasterExit;
+    public Vector2 ShipmasterExit;
+    public Vector2 FirstmateExit;
+
+    public override void DoSpecialAction(string actionTag) {
+        switch (actionTag) {
+            case "QuartermasterExit":
+                npcExit(Quartermaster, QuartermasterExit);
+                if (gameObject.GetComponent<Interactable>().Debugging) { Debug.Log("Exit Quartermaster"); }
+                break;
+            case "ShipmasterExit":
+                npcExit(Shipmaster, ShipmasterExit);
+                if (gameObject.GetComponent<Interactable>().Debugging) { Debug.Log("Exit Shipmaster"); }
+                break;
+            case "FirstmateExit":
+                npcExit(Firstmate, FirstmateExit);
+                if (gameObject.GetComponent<Interactable>().Debugging) { Debug.Log("Exit Firstmate"); }
+                break;
+            default:
+                if (gameObject.GetComponent<Interactable>().Debugging) { Debug.Log(actionTag + " isn't defined in SpecialActions_Cutscene_Handler."); }
+                break;
+        }
+    }
+
+    private void npcExit(GameObject npc, Vector2 exit) {
+        Destroy(npc);
+    }
+}
