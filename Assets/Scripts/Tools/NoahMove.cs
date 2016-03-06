cc398e166436480c669b4c3c2bdfb59c2ae55636
NC8815 <nopc15@hampshire.edu>
2016-03-06 18:59:38 -0500

    sandbox work

---
+++ Assets/Scripts/Tools/NoahMove.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoahMove : MonoBehaviour {
+
+	private Vector3 destination;
+	private NavMeshAgent navAgent;
+
+
+	// Use this for initialization
+	void Start () {
+		navAgent = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void GoTo(Vector3 targetPoint){
+		navAgent.SetDestination(targetPoint);
+	}
+}
---
+++ Assets/Scripts/Tools/NoahNavPlane.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class NoahNavPlane : MonoBehaviour, IPointerClickHandler {
+
+	NavMeshAgent Player;
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.Find ("Sadie").GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseUp(){
+		//Debug.Log ("Mouse UP");
+	}
+
+	public void OnPointerClick (PointerEventData eventData)
+	{
+		NavMeshPath path = new NavMeshPath ();
+		Vector3 destination = eventData.pointerCurrentRaycast.worldPosition;
+		Player.GetComponent<NavMeshAgent> ().CalculatePath (destination, path);
+		if(path.status == NavMeshPathStatus.PathComplete){
+			Player.SetDestination (destination);
+		}
+	}
+}
