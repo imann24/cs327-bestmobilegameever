5c25521fc53d1521272ab53aff0cd4f8f207d8f0
Donna <pan23h@mtholyoke.edu>
2016-03-06 01:43:43 -0500

    Door final
    Sorry about changing the Unity Scene. However, there should be no
    errors.

---
+++ Assets/Scripts/Tools/SpecialActions/SpecialActions_Door.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SpecialActions_Door : SpecialActions {
+
+	public override void DoSpecialAction(string actionTag) {
+		switch (actionTag) {
+		case "SoundOpenDoor":
+			EventController.Event("DoorOpen");
+			break;
+		default:
+			if (gameObject.GetComponent<Interactable>().Debugging) { Debug.Log(actionTag + " isn't defined in SpecialActions_Door."); }
+			break;
+		}
+	}
+}
