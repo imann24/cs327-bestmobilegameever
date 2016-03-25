52c5f919d244fdae7de6535cbd212b16a6f45c12
Sienna Cornish <sc16@hampshire.edu>
2016-03-25 14:25:10 -0400

    Adding narrative assets.

--- Assets/Scripts/Tools/SpecialActions/SpecialActions_Swabbie.cs
+++ Assets/Scripts/Tools/SpecialActions/SpecialActions_Swabbie.cs
@@ -6,14 +6,12 @@ public class SpecialActions_Swabbie : SpecialActions {
     public override void DoSpecialAction(string actionTag) {
         switch (actionTag) {
             case "SwabbieFlee":
-                Debug.Log("Invoking destroy...");
                 Invoke("destroyMe", 3f);
                 break;
         }
     }
 
     private void destroyMe() {
-        Debug.Log("Attempting to destroy...");
         Destroy(gameObject);
     }
 }
