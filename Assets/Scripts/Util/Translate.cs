e60a91a30af9cf069025c697de620dbfd1e977f3
imann24 <contact@isaiahmann.com>
2016-05-07 18:03:12 -0400

    Fixing rags z-position in world

---
+++ Assets/Scripts/Util/Translate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Translate : MonoBehaviour {
+	public Vector3 Translation;
+	public float WaitTime = 5f;
+	void Start () {
+		StartCoroutine(TimedTranslate());
+	}
+
+	IEnumerator TimedTranslate () {
+		yield return new WaitForSeconds(WaitTime);
+		transform.position += Translation;
+	}
+}
