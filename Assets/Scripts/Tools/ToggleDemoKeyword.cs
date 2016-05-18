ecb2999dd8ade4992e4385bbf1fa8b1c00e1b3b4
imann24 <contact@isaiahmann.com>
2016-05-18 07:48:41 -0400

    Adding easy toggle to remove demo text from in game

---
+++ Assets/Scripts/Tools/ToggleDemoKeyword.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System.Collections;
+
+public class ToggleDemoKeyword : MonoBehaviour {
+	public bool ReplaceWithCustomText;
+
+	[HideInInspector]
+	public string CustomText;
+
+	void Awake () {
+		if (!GameTextUtil.ShowDemoKeyword) {
+			Text myText = GetComponent<Text>();
+			if (ReplaceWithCustomText) {
+				myText.text = myText.text = CustomText;
+			} else {
+				myText.text = myText.text.Replace(GameTextUtil.DemoKeyword, string.Empty);
+			}
+		}
+	}
+
+
+
+}
+
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(ToggleDemoKeyword))]
+public class ToggleDemoKeywordEditor : Editor {
+	public override void OnInspectorGUI() {
+		var thisScript = target as ToggleDemoKeyword;
+		if (thisScript) {
+			thisScript.ReplaceWithCustomText = GUILayout.Toggle(thisScript.ReplaceWithCustomText, "Use Custom Text");
+			if (thisScript.ReplaceWithCustomText) {
+				thisScript.CustomText = EditorGUILayout.TextField(thisScript.CustomText);
+			}
+		}
+	}
+}
+#endif
---
+++ Assets/Scripts/Util/GameTextUtil.cs
@@ -0,0 +1,5 @@
+﻿public static class GameTextUtil {
+
+	public const bool ShowDemoKeyword = true;
+	public const string DemoKeyword = " Demo";
+}
